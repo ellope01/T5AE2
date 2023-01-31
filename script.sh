7a41e7ab03be2ecfed1b8ee6359b98c112b67fa3
RosaBosch <roboma@floridauniversitaria.com>
2023-01-31 15:51:36 +0100

    Introducción del script

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Bienvenido al juego de los puñitos."
+echo "==================================="
+
+echo "Comenzaremos con LAS NORMAS e INSTRUCCIONES"
+echo "1. Se intentará descubrir el numero de piedras al menos de un jugador."
+echo "2. "
+ 
