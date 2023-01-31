6fb9e42741510da49e13b02da70a8d2dc5e4c2fc
RosaBosch <roboma@floridauniversitaria.com>
2023-01-31 15:54:35 +0100

    Introducir segunda instrucción

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 echo "Bienvenido al juego de los puñitos."
 echo "==================================="
 
-echo "Comenzaremos con LAS NORMAS e INSTRUCCIONES"
+echo "Comenzaremos con LAS NORMAS e INSTRUCCIONES:"
 echo "1. Se intentará descubrir el numero de piedras al menos de un jugador."
-echo "2. "
+echo "2. Habrá 5 intentos, si se acierta superior a 3 intentos se gana y menos de 3 intentos pierdes."
+echo ""
  
