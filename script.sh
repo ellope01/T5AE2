1402323bdccd5b2d0abf3e7b0d3fadaa9ddbd9c5
Elizabeth Lopez <ellope01@floridauniversitaria.es>
2023-01-31 15:59:06 +0100

    Norma 3

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Bienvenido al juego de los puñitos."
+echo "==================================="
+
+echo "Comenzaremos con LAS NORMAS e INSTRUCCIONES:"
+echo "1. Se intentará descubrir el numero de piedras al menos de un jugador."
+echo "2. Habrá 5 intentos, si se acierta superior a 3 intentos se gana y menos de 3 intentos pierdes."
+echo "3. Habrá 20 piedras como máximo"
+ 
