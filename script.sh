29efc8a3db6eb7bb9c9eb6ffa0e2f5d1b27c8816
Óliver Puchades Pinel <114495024+Ol1v3r27@users.noreply.github.com>
2023-01-31 16:42:01 +0100

    Comienzo Juego

--- script.sh
+++ script.sh
@@ -6,3 +6,13 @@ echo "Comenzaremos con LAS NORMAS e INSTRUCCIONES:"
 echo "1. Se intentará descubrir el numero de piedras al menos de un jugador."
 echo "2. Habrá 5 intentos, si se acierta superior a 3 intentos se gana y menos de 3 intentos pierdes."
 echo "3. Habrá 20 piedras como máximo"
+
+read -p "¿Deseas jugar (S/N)?: " comienzo
+if [ $comienzo = 'S' ]
+then
+echo ""
+echo "Juego"
+else
+echo ""
+echo "¡Gracias por jugar! Por favor, ejecuta el programa otra vez para volver a jugar."
+fi
