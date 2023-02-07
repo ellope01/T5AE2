3fdccf3a6099ef3ab9e318bb1aa917df5f804cf7
Elizabeth <ellope01@floridauniversitaria.es>
2023-02-07 16:26:26 +0100

    versiob 4

--- script.sh
+++ script.sh
@@ -8,14 +8,8 @@ echo "2. Habrá 5 intentos, si se acierta superior a 3 intentos se gana y menos 
 echo "3. Habrá 20 piedras como máximo"
 
 read -p "¿Deseas jugar (S/N)?: " comienzo
-while [ $comienzo=='S' ] || [ $comienzo=='N' ];
+while [ $comienzo =='S' ] 
 do
-if [ $comienzo=='S' ]
-    then
-    echo "Has aceptado jugar."
-    jugar=true
-while [ $jugar==true ];
-do 
     read -p " Introduce un número de jugadores (máximo 5): " jugadores
 
     if [ $jugadores -gt 5 ]; 
@@ -40,60 +34,33 @@ do
         sum=$((sum + random))
     done
 
-echo "Adivina los números individuales de los jugadores:"
-correcto=0
-for(( i=1; i<=$jugadores; i++ ));
-do
-    read -p "Jugador $i: " adivinar 
-    if [ $adivinar -eq ${numJugador[$i]} ];
-        then
+    echo "Adivina los números individuales de los jugadores:"
+    correcto=0
+    for(( i=1; i<=$jugadores; i++ ));
+        do
+        read -p "Jugador $i: " adivinar 
+        if [ $adivinar -eq ${numJugador[$i]} ];
+            then
             correcto=$((correcto+1))
+        fi
+    done
+
+    if [ $correcto -gt $((jugadores%2)) ]; 
+    then
+        echo "¡Felicidades has ganado!"
+    else 
+        echo "Lo siento, has perdido."
     fi
-done
-
-if [ $correcto -gt $((jugadores%2)) ]; 
-    then
-        echo "¡Felicidades has ganado!"
-    else 
-        echo "Lo siento, has perdido."
-fi
  
-read -p "Adivina la suma de los números:" adivinar_suma
+    read -p "Adivina la suma de los números:" adivinar_suma
 
-if [ $adivinar_suma -eq $sum ];
+    if [ $adivinar_suma -eq $sum ];
     then
         echo "¡Felicidades has ganado!"
     else 
         echo "Lo siento, has perdido."
-fi
+    fi
 
-
-read -p "¿Quieres volver a jugar?(S/N):" jugar_otra_vez
-
-# Bucle de volver a jugar.
-while [ $jugar_otra_vez=='S' ] || [ $jugar_otra_vez=='N' ];
-do
- if [ $jugar_otra_vez=='S' ]
-    then
-    echo "Has aceptado volver a jugar."
-    $comienzo
-
- elif [ $jugar_otra_vez=='N' ]
-    then
-    echo "No has aceptado volver a jugar. Vuelve pronto!"
-    exit
- fi
-done
-    echo "Error, debes introducir (S / N). Gracias."
-    exit   
+read -p "¿Quieres volver a jugar?(S/N):" comienzo
 
 done
-# Bucle de Fin y No de jugar.
-elif [ $comienzo=='N' ]
-then
-    echo "No has aceptado a jugar. Vuelve pronto!"
-    exit
-fi
-done
-    echo "Error, debes introducir (S / N). Gracias."
-    exit
