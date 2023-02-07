3f185a64728131d73476c6b073768937d401e17a
Óliver Puchades Pinel <114495024+Ol1v3r27@users.noreply.github.com>
2023-02-07 17:01:58 +0100

    Version 5

--- script.sh
+++ script.sh
@@ -1,74 +1,101 @@
 #!/bin/bash
+
+# Reglas del juego.
 echo "Bienvenido al juego de los puñitos."
 echo "==================================="
 
 echo "Comenzaremos con LAS NORMAS e INSTRUCCIONES:"
 echo "1. Se intentará descubrir el numero de piedras al menos de un jugador."
-echo "2. Habrá 5 intentos, si se acierta superior a 3 intentos se gana y menos de 3 intentos pierdes."
+echo "2. Habrá 2 intentos para adivinar."
 echo "3. Habrá 20 piedras como máximo"
 
+# Entrada al juego.
 read -p "¿Deseas jugar (S/N)?: " comienzo
-if [ $comienzo = 'S' ]
-then
-echo ""
- jugar_de_nuevo=true
 
-while [$jugar_de_nuevo==true];do 
-echo " Introduce el número de jugadores (máximo 5): "
-read jugadores
+while [ $comienzo == 'S' ] || [ $comienzo == 'N' ];
+do
+if [ $comienzo == 'S' ]
+    then
+    echo "Has aceptado jugar."
+    jugar=true
 
-if [ $jugadores -gt 5 ]; then
-echo "El numero de jugadores debe ser menor o igual a 5."
-exit 1
-fi
-echo "Introduce el número límite de piedras/(entre 1 y 20):"
-read piedras
+while [ $jugar == true ];
+do 
+    read -p " Introduce un número de jugadores (máximo 5): " jugadores
 
-if [$piedras -lt 1]||[$piedras -gt 20];
-then 
-echo "El número minimo debe ser entre 1 y 20"
-exit 1
-fi
+    if [ $jugadores -gt 5 ]; 
+        then
+            echo "El numero de jugadores debe ser menor o igual a 5."
+            exit 1
+    fi
 
-sum=0
-for((i=1;i<=$jugadores;i++));do 
-random=$((RANDOM%limit +1))
-numero_jugador[$i]=$random
-sum=$((sum + random))
-done
+    read -p "Introduce un número límite de piedras (mín. 1 / máx. 20): " piedras
 
-echo "Adivina los números individuales de los jugadores:"
+    if [ $piedras -lt 1 ] || [ $piedras -gt 20 ];
+        then 
+            echo "El número mínimo debe ser entre 1 y 20"
+            exit 1
+    fi
+
+    sum=0
+    for(( i = 1; i <= $jugadores; i++ ));
+    do 
+        random=$((RANDOM % $piedras +1))
+        numJugador[$i]=$random
+        sum=$((sum + random))
+    done
+
+echo "Adivina los números individuales de los jugadores: "
 correcto=0
-for((i=1;i<=$jugadores;i++));do
-echo "Jugador $i:"
-read adivinar 
-if [$adivinar -eq ${numero_jugador[$i]}];then
-correcto=$((correcto+1))
-fi
+
+for(( i = 1; i <= $jugadores; i++ ));
+do
+    read -p "Jugador $i: " adivinar 
+    if [ $adivinar -eq ${numJugador[$i]} ];
+        then
+            correcto=$((correcto+1))
+    fi
 done
 
-if [$correcto -gt $((jugadores%2))]; then
-echo "Has ganado"
-else 
-echo "Perdiste"
+if [ $correcto -gt $((jugadores % 2)) ]; 
+    then
+        echo "¡Felicidades has ganado!"
+    else 
+        echo "Lo siento, has perdido, no has adivinado las piedras de los jugadores."
 fi
- 
-echo "Adivina la suma de los números:"
-read adivinar_suma
 
-if [$adivinar_suma -eq $sum];then
-echo "Has ganado"
-else 
-echo "Has perdido"
-fi
-echo "¿Quieres volver a jugar?(S/N):"
-read jugar_otra_vez
+read -p "Adivina la suma de los números: " adivinar_suma
 
-if [$jugar_otra_vez == "S"];
-then 
-jugar_de_nuevo=true
-else 
-jugar_de_nuevo=false
-echo "¡Gracias por jugar! Por favor, ejecuta el programa otra vez para volver a jugar."
-done 
+if [ $adivinar_suma -eq $sum ];
+    then
+        echo "¡Felicidades! Has acertado la suma de los números."
+    else 
+        echo "Lo siento, has perdido, no has adivinado la suma de los números."
 fi
+
+
+read -p "¿Quieres volver a jugar?(S/N):" comienzo
+
+ if [ $comienzo == 'S' ]
+    then
+    echo "Has aceptado volver a jugar."
+ elif [ $comienzo == 'N' ]
+    then
+    echo "No has aceptado volver a jugar. Vuelve pronto!"
+    exit
+fi
+   
+while [ $comienzo != 'S' ] && [ $comienzo != 'N' ]
+do
+    echo "Error, debes introducir (S / N). Gracias."
+    read -p "¿Deseas jugar (S/N)?: " comienzo
+done
+
+done
+
+elif [ $comienzo == 'N' ]
+then
+    echo "No has aceptado a jugar. Vuelve pronto!"
+    exit
+fi
+done
